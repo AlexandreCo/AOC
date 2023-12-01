d5fe5255046227f361a76adb19005a1c904777c8
AlexandreCo <alexandre.github@gmail.com>
2023-12-01 09:36:13 +0100

    AOC 01/12/2023 2 Trebuchet

--- main.py
+++ main.py
@@ -1,19 +1,50 @@
 import re
 
-# Press the green button in the gutter to run the script.
+def spelledToNumber(line):
+    line = line.replace(str("one"), "o1")
+    line = line.replace(str("two"), "t2")
+    line = line.replace(str("three"), "t3")
+    line = line.replace(str("four"), "f4")
+    line = line.replace(str("five"), "f5")
+    line = line.replace(str("six"), "s6")
+    line = line.replace(str("seven"), "s7")
+    line = line.replace(str("eight"), "e8")
+    line = line.replace(str("nine"), "n9")
+    return line
+
+def spelledToNumberRevers(line):
+    line = line.replace(str("one")[::-1], "e1")
+    line = line.replace(str("two")[::-1], "o2")
+    line = line.replace(str("three")[::-1], "e3")
+    line = line.replace(str("four")[::-1], "r4")
+    line = line.replace(str("five")[::-1], "e5")
+    line = line.replace(str("six")[::-1], "x6")
+    line = line.replace(str("seven")[::-1], "n7")
+    line = line.replace(str("eight")[::-1], "t8")
+    line = line.replace(str("nine")[::-1], "e9")
+    return line
+
+
+def removeLetter(line) :
+    return re.sub("[a-z]", "", line)
+
 if __name__ == '__main__':
     file = open('./1.txt', "r")
     lines = file.readlines()
     file.close()
-    sum=0
+    sum = 0
     for rline in lines:
-        line=rline
-        line = re.sub("[a-z]", "", line)
-        number=(int(line[0])*10+int(line[len(line)-2]));
-        sum+=number
-        print(rline,line,line[0],line[len(line)-2],number,sum)
+        # get first number
+        line=rline.strip()
+        line=spelledToNumber(line)
+        line = removeLetter(line)
+        first=int(line[0])
 
+        line=rline [::-1].strip()
+        line=spelledToNumberRevers(line)
+        line = removeLetter(line)
+        second=int(line[0])
 
-
-# 54644	sans sub
-# 52963	avec sub
+        number = int(first) * 10 +int(second)
+        sum += number
+    print(sum)
