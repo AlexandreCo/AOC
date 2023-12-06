e8a08c13fc1dbbf272e55586952bd9aea7c7d0be
AlexandreCo <alexandre.github@gmail.com>
2023-12-06 18:39:33 +0100

    Day 2 : display part 1 and 2

--- day2.py
+++ day2.py
@@ -7,41 +7,14 @@ class Color(Enum):
     BLUE = 3
 
 Color = Enum('Color', ['RED', 'GREEN', 'BLUE'])
-def spelledToNumber(line):
-    line = line.replace(str("one"), "o1")
-    line = line.replace(str("two"), "t2")
-    line = line.replace(str("three"), "t3")
-    line = line.replace(str("four"), "f4")
-    line = line.replace(str("five"), "f5")
-    line = line.replace(str("six"), "s6")
-    line = line.replace(str("seven"), "s7")
-    line = line.replace(str("eight"), "e8")
-    line = line.replace(str("nine"), "n9")
-    return line
-
-def spelledToNumberRevers(line):
-    line = line.replace(str("one")[::-1], "e1")
-    line = line.replace(str("two")[::-1], "o2")
-    line = line.replace(str("three")[::-1], "e3")
-    line = line.replace(str("four")[::-1], "r4")
-    line = line.replace(str("five")[::-1], "e5")
-    line = line.replace(str("six")[::-1], "x6")
-    line = line.replace(str("seven")[::-1], "n7")
-    line = line.replace(str("eight")[::-1], "t8")
-    line = line.replace(str("nine")[::-1], "e9")
-    return line
-
-
-def removeLetter(line) :
-    return re.sub("[a-z]", "", line)
-
 
 class cube:
-    max=0
-    nbCube=0
-    max_bag=0
+
     def __init__(self, max_bag):
         self.max_bag=max_bag
+        self.max=0
+        self.nbCube=0
+
     def draw(self, newCubes):
         self.nbCube = self.nbCube + newCubes
         if( newCubes > self.max ):
@@ -63,16 +36,9 @@ class cube:
         else:
             return True
 
-
-if __name__ == '__main__':
-
-    file = open('./2.txt', "r")
-    lines = file.readlines()
-    file.close()
+def run(part):
     sum = 0
     nbline = 0
-    #only 12 red cubes, 13 green cubes, and 14 blue cubes
-
     blue=cube(14)
     red=cube(12)
     green=cube(13)
@@ -82,16 +48,11 @@ if __name__ == '__main__':
         blue.reset()
         red.reset()
         green.reset()
-        #print("line", line)
         sublines=line.split(":")
         for subline in sublines :
-            #print(subline)
             subsets=subline.split(";")
             for subset in subsets:
                 draws = subset.split(",")
-                nbBlue = 0
-                nbRed = 0
-                nbGreen = 0
                 for draw in draws:
                     objs=draw.split(" ")
                     if(objs[0] != "Game"):
@@ -103,12 +64,17 @@ if __name__ == '__main__':
                             red.draw(nb)
                         if color == "green":
                             green.draw(nb)
-        #print(blue.getMax(),red.getMax(),green.getMax())
-        #print(blue.valid(),red.valid(),green.valid())
-        power=blue.getMax()*red.getMax()*green.getMax();
-        #print(power)
-        sum+=power
+        if(part==2):
+            power=blue.getMax()*red.getMax()*green.getMax();
+            sum+=power
+        if(part==1):
+            if (blue.valid() & red.valid() & green.valid()):
+                sum += nbline
+    return sum
+if __name__ == '__main__':
 
-
-
-    print(sum)
+    file = open('./2.txt', "r")
+    lines = file.readlines()
+    file.close()
+    print(f"Part 1 : {run(1)}")
+    print(f"Part 2 : {run(2)}")
