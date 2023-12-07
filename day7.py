c0a80068b01ddbaea53aa654b8423c9770c8d8c5
AlexandreCo <alexandre.github@gmail.com>
2023-12-07 13:57:56 +0100

    AOC 07/12/2023 2 Camel Cards

--- day7.py
+++ day7.py
@@ -1,11 +1,9 @@
 import re
 import sys
-debug=0
-
 class Game :
 
 
-    def __init__(self,debug,hand,bid,valDict):
+    def __init__(self,debug,hand,bid,valDict,part):
         self.debug=debug
         self.score=0
         self.hand=hand
@@ -15,7 +13,7 @@ class Game :
         self.handName = 'None'
         self.handValue = 0
         self.valDict=valDict
-
+        self.part = part
         self.typeDict = {
             'None' : 0,
             'HighCard' : 1,
@@ -34,31 +32,37 @@ class Game :
             else:
                 self.cardsValue.append(self.valDict[c])
 
-
-        if(self.isFiveOf()):
+        if(self.isFiveOf((self.part==2))):
             self.handName = 'FiveOf'
         else:
-            if(self.isFourOf()):
+            if(self.isFourOf((self.part==2))):
                 self.handName = 'FourOf'
             else:
-                if(self.isFullHouse()):
+                if(self.isFullHouse((self.part==2))):
                     self.handName = 'FullHouse'
                 else:
-                    if(self.isThreeOf()):
+                    if(self.isThreeOf((self.part==2))):
                         self.handName = 'ThreeOf'
                     else:
-                        if(self.isOnePair()):
-                            self.handName = 'OnePair'
+                        if (self.isTwoPair((self.part == 2))):
+                            self.handName = 'TwoPair'
                         else:
-                            if(self.isTwoPair()):
-                                self.handName = 'TwoPair'
+                            if (self.isOnePair((self.part == 2))):
+                                self.handName = 'OnePair'
+                            else:
+                                self.handName = 'HighCard'
 
         self.setHandValue()
         if(self.debug):
-            print(self.cards,self.cardsValue,self.bid,end='')
-            print(f" {self.handName} {hex(self.handValue)}")
+            print(f"{self.hand} ", end="")
+            print(f"{self.bid} ", end="")
+            print(f"{self.handName} {hex(self.handValue)}",end="")
+            if(self.debug > 1):
+                print(f" ({self.getMaxValueInHand()}) ",end="")
+            print(f"")
 
         assert self.handValue!=0
+        assert self.typeDict[self.handName] != 0
 
     def __lt__(self, other):
         return self.handValue < other.handValue
@@ -75,57 +79,94 @@ class Game :
             mult/=0x10
         return int(strongValue)
 
-    def isFiveOf(self):
+    def isFiveOf(self,joke):
         ret=False
-        nb, value = self.getMaxValueInHand()
-        if(nb[0]==5):
+        nb,nbj = self.getMaxValueInHand()
+        if(nb[0]>=5):
             ret=True
+        if(joke):
+            if(self.isFourOf(False) and  (nbj>0)):
+                ret=True
+            if(self.isThreeOf(False) and (nbj>1)):
+                if(nbj!=3):
+                    ret=True
+                else:
+                    if(self.isFullHouse(True)):
+                        ret = True
+            if(self.isTwoPair(False) and (nbj>2)):
+                ret=True
         return ret
 
-    def isFourOf(self):
+    def isFourOf(self,joke):
         ret = False
-        nb, value = self.getMaxValueInHand()
+        nb,nbj =  self.getMaxValueInHand()
         if (nb[0] == 4):
             ret = True
+        if(joke):
+            if(self.isThreeOf(False) and (nbj>0)):
+                ret=True
+            if(self.isTwoPair(False) and (nbj>1)):
+                ret=True
         return ret
 
-    def isFullHouse(self):
+    def isFullHouse(self,joke):
         ret = False
-        nb, value = self.getMaxValueInHand()
+        nb,nbj = self.getMaxValueInHand()
         if (nb[0] == 3):
             if (nb[1] == 2):
                 ret = True
+        if(joke):
+            if(self.isTwoPair(False) and (nbj>0)):
+                ret=True
         return ret
 
-    def isThreeOf(self):
+    def isThreeOf(self,joke):
         ret = False
-        nb, value = self.getMaxValueInHand()
+        nb,nbj = self.getMaxValueInHand()
         if (nb[0] == 3):
-            if (nb[1] == 1):
+            ret = True
+        if (joke):
+            if (self.isOnePair(False) and (nbj > 0)):
                 ret = True
         return ret
 
-    def isTwoPair(self):
+    def isTwoPair(self,joke):
         ret = False
-        nb, value = self.getMaxValueInHand()
+        nb,nbj = self.getMaxValueInHand()
         if (nb[0] == 2):
             if (nb[1] == 2):
                 ret = True
+        if (joke):
+            if (self.isOnePair(False) and (nbj > 0)):
+                ret = True
         return ret
 
-    def isOnePair(self):
+    def isOnePair(self,joke):
         ret = False
-        nb, value = self.getMaxValueInHand()
+        nb,nbj = self.getMaxValueInHand()
         if (nb[0] == 2):
             if (nb[1] == 1):
                 ret = True
+        if (joke):
+            if(nbj > 0):
+                ret = True
         return ret
+
+
 
     def getMaxValueInHand(self):
         max=0
         card=0
         nbof=[]
         tab = []
+        nbJ=0
+        def incValue(value,max,card):
+            tab[value] += 1
+            if (tab[value] > max):
+                max = tab[value]
+                card = value
+            return max,card
+
         for i in range(15):
             tab.append(0)
         for cardValue in self.cardsValue:
@@ -138,10 +179,10 @@ class Game :
             if(tab[i]):
                 nbof.append(tab[i])
         nbof.sort(reverse=True)
-        return nbof,card
+        nbJ=tab[self.valDict['J']]
+        return nbof,nbJ
 
-def getData(filename,games,part):
-    ldebug=debug
+def getData(filename,games,part,debug):
     valDict1 = {
         'T': 10,
         'J': 11,
@@ -167,11 +208,11 @@ def getData(filename,games,part):
     file.close()
     for line in lines:
         hand,bid=line.strip().split(" ")
-        games.append(Game(ldebug,hand,int(bid),valDict))
+        games.append(Game(debug,hand,int(bid),valDict,part))
 
 
-def run(cards,part):
-    ldebug = debug
+def run(debug):
+
     result=0
     games.sort()
     rank=0
@@ -179,23 +220,28 @@ def run(cards,part):
     for game in games:
         rank+=1
         result+=rank*game.bid
-        if(ldebug):
+        if(debug):
             print(rank,game.cards,game.bid)
+        #print(f"{rank}; {game.hand}; {game.handName}; {game.handValue}; {game.bid};")
     return result
 
 
 
 if __name__ == '__main__':
     debug = 0
-    filename='./7-tiny.txt'
+    #filename='./7-single.txt'
     filename = './7.txt'
-    games = []
-    getData(filename,games,1)
-    print(f"Part 1 : {run(games,1)}")
+
+    part=1
 
     games = []
-    getData(filename,games,0)
-    print(f"Part 2 : {run(games,2)}")
+    getData(filename,games,part,debug)
+    print(f"Part 1 : {run(debug)}")
+
+    games = []
+    part+=1
+    getData(filename,games,part,debug)
+    print(f"Part 2 : {run(debug)}")
 
 
 
